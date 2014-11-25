b3d78a5170dfd93dd127a81bbd4c5d4472633ea0
Jonathan Harper <jharper@eecs.berkeley.edu>
2014-11-25 14:48:38 -0800

    update Schema model for this application

---
+++ src/Schema.js
@@ -0,0 +1,99 @@
+var _ = require('underscore');
+
+function Schema(data, attrs, nodeAttrs, ids, mappings) {
+    this.data = data;
+    this.attrs = attrs;
+
+    var columnData = [];
+    _.each(data, function(val, key) {
+        columnData.push({
+            name: key,
+            data: val
+        });
+    });
+    _.each(attrs, function(val, key) {
+        columnData.push({
+            name: key,
+            data: val
+        });
+    });
+
+    this.numFields = Object.keys(data).length;
+    this.ids = ids;
+    this.mappings = mappings;
+    this.nodeAttrs = nodeAttrs;
+}
+
+Schema.prototype.attrIsMapped = function(attr) {
+    return _.find(this.mappings, function(mapping) {
+        return mapping.attr == attr;
+    }) !== undefined;
+};
+
+Schema.prototype.uniqVals = function(fieldName, isAttr) {
+    var allVals;
+    if (isAttr) {
+        allVals = this.attrs[fieldName];
+    }
+    else {
+        allVals = this.data[fieldName];
+    }
+    return _.uniq(allVals);
+};
+
+Schema.prototype.updateMarks = function(val, attr, ids) {
+    var schema = this;
+    _.each(ids, function(id) {
+        var ind = _.find(schema.ids[id]);
+        schema.attrs[attr][ind] = val;
+
+        if (attr === "area") {
+            schema.attrs["width"] = Math.sqrt(val);
+        }
+        else if (attr === "width" || attr === "height") {
+            schema.attrs["area"] = schema.attrs["width"][ind]
+            * schema.attrs["height"][ind];
+        }
+    });
+};
+
+Schema.fromJSON = function(deconData) {
+    return new Schema(
+        deconData.data,
+        deconData.attrs,
+        deconData.nodeAttrs,
+        deconData.ids,
+        deconData.mappings
+    );
+};
+
+Schema.prototype.getDataCSVBlob = function() {
+    var keys = Object.keys(this.data);
+    console.log(keys);
+    var dataLen = this.data[keys[0]].length;
+    var dataRows = [];
+
+    dataRows.push(keys.join(","));
+
+    for (var i = 0; i < dataLen; ++i) {
+        var dataRow = [];
+        for (var j = 0; j < keys.length; ++j) {
+            var dataVal = this.data[keys[j]][i];
+            if (typeof dataVal === "string") {
+                dataRow.push(dataVal.replace(",", ""));
+            }
+            else {
+                dataRow.push(dataVal);
+            }
+        }
+        console.log(dataRow);
+        dataRow = dataRow.join(",");
+        dataRows.push(dataRow);
+    }
+
+    dataRows = dataRows.join("\n");
+
+    return dataRows;
+};
+
+module.exports = Schema;
