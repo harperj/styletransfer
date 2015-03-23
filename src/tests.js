3a44329307795df50200f200da56a8d1dd2a3c1a
Jonathan Harper <jharper@eecs.berkeley.edu>
2015-03-22 20:57:53 -0700

    Added excel bar chart.  Only partially functional

--- src/tests.js
+++ src/tests.js
@@ -4,16 +4,24 @@ var tests = [
         target_file: './data/vallandingham_scatter.json'
     },
     {
-        target_file: './data/weiglemc_scatter.json',
-        source_file: './data/vallandingham_scatter.json'
+        source_file: './data/vallandingham_scatter.json',
+        target_file: './data/weiglemc_scatter.json'
     },
     {
         source_file: './data/dowson_bars.json',
         target_file: './data/mbostock_bars.json'
     },
     {
-        target_file: './data/dowson_bars.json',
-        source_file: './data/mbostock_bars.json'
+        source_file: './data/mbostock_bars.json',
+        target_file: './data/dowson_bars.json'
+    },
+    {
+        source_file: './data/mbostock_bars.json',
+        target_file: './data/excel_bars.json'
+    },
+    {
+        source_file: './data/excel_bars.json',
+        target_file: './data/mbostock_bars.json'
     },
     {
         source_file: './data/leondutoit_bars.json',
@@ -24,12 +32,12 @@ var tests = [
         target_file: './data/mbostock_neg_bars.json'
     },
     {
-        target_file: './data/leondutoit_bars.json',
-        source_file: './data/mbostock_bars_3.json'
+        source_file: './data/mbostock_bars_3.json',
+        target_file: './data/leondutoit_bars.json'
     },
     {
-        target_file: './data/leondutoit_bars.json',
-        source_file: './data/mbostock_bars_3.json'
+        source_file: './data/mbostock_bars_3.json',
+        target_file: './data/leondutoit_bars.json'
     },
     {
         source_file: './data/mbostock_bars.json',
