a5637edd70cfc88b4d196c856349491095918c9b
Jonathan Harper <jharper@eecs.berkeley.edu>
2015-04-11 14:35:38 -0700

    Code checkin.  Removal of old, commented out code.  Updated deconstructions.  New tests file for "similar aspect" only

---
+++ src/tests-similaraspect.js
@@ -0,0 +1,20 @@
+var tests = [
+    //{
+    //    source_file: './data/weiglemc_scatter.json',
+    //    target_file: './data/vallandingham_scatter.json'
+    //},
+    //{
+    //    source_file: './data/vallandingham_scatter.json',
+    //    target_file: './data/weiglemc_scatter.json'
+    //},
+    //{
+    //    source_file: './data/mbostock_bars.json',
+    //    target_file: './data/vallandingham_scatter.json'
+    //},
+    {
+        source_file: './data/5308_bars.json',
+        target_file: './data/mbostock_bars.json'
+    }
+];
+
+module.exports = tests;
