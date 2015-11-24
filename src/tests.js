29dd6943e036bd92c8a16524b6da551106b74a0e
Harper <harper@persistentefficiency.com>
2015-11-23 23:29:43 -0800

    Add new tests for teaser figure

--- src/tests.js
+++ src/tests.js
@@ -1,13 +1,13 @@
 var tests = [
-    {
-        source_file: './data/result_scatter_1.json',
-        target_file: './data/anscombe1.json'
-    },
-    {
-        source_file: './data/simplebars.vegalite.json',
-        source_type: 'vegalite',
-        target_file: './data/dowson_bars.json'
-    },
+    //{
+    //    source_file: './data/result_scatter_1.json',
+    //    target_file: './data/anscombe1.json'
+    //},
+    //{
+    //    source_file: './data/simplebars.vegalite.json',
+    //    source_type: 'vegalite',
+    //    target_file: './data/dowson_bars.json'
+    //},
      //CARS ROLLUP EXAMPLES
     {
         source_file: './data/cars_rollup.data.json',
@@ -24,119 +24,134 @@ var tests = [
         source_type: 'json_data',
         target_file: './data/dowson_bars.json'
     },
-     //IRIS EXAMPLES
     {
-        source_file: './data/iris.data.json',
+        source_file: './data/cars_rollup2.data.json',
         source_type: 'json_data',
-        target_file: './data/vallandingham_scatter.json'
+        target_file: './data/result_bars_1.json'
     },
     {
-        source_file: './data/iris.data.json',
+        source_file: './data/cars_rollup3.data.json',
         source_type: 'json_data',
-        target_file: './data/weiglemc_scatter_2.json'
-    },
-    {
-        source_file: './data/iris.data.json',
-        source_type: 'json_data',
-        target_file: './data/leondutoit_dots.json'
-    },
-    {
-        source_file: './data/barley.data.json',
-        source_type: 'json_data',
-        target_file: './data/leondutoit_dots.json'
-    },
-    {
-        source_file: './data/purple_scatter.json',
-        target_file: './data/vallandingham_scatter.json'
-    },
-    {
-        source_file: './data/weiglemc_scatter_2.json',
-        target_file: './data/vallandingham_scatter.json'
-    },
-    {
-        source_file: './data/vallandingham_scatter.json',
-        target_file: './data/weiglemc_scatter_2.json'
-    },
-    {
-        source_file: './data/mbostock_bars.json',
-        target_file: './data/vallandingham_scatter.json'
-    },
-    {
-        source_file: './data/dowson_bars.json',
-        target_file: './data/mbostock_bars.json'
-    },
-    {
-        source_type: 'json_data',
-        source_file: './data/iris.data.json',
-        target_file: './data/mbostock_bars.json'
-    },
-    {
-        source_type: 'json_data',
-        source_file: './data/iris.data.json',
-        target_file: './data/dowson_bars.json'
-    },
-    {
-        source_file: './data/mbostock_bars.json',
-        target_file: './data/dowson_bars.json'
-    },
-    {
-        source_file: './data/weiglemc_scatter.json',
-        target_file: './data/dowson_bars.json'
-    },
-    {
-        source_file: './data/dowson_bars.json',
-        target_file: './data/weiglemc_scatter.json'
-    },
-    {
-        source_file: './data/mbostock_bars.json',
-        target_file: './data/excel_bars.json'
-    },
-    {
-        source_file: './data/excel_bars.json',
-        target_file: './data/mbostock_bars.json'
-    },
-    {
-        source_file: './data/mbostock_bars.json',
-        target_file: './data/leondutoit_dots.json'
-    },
-    {
-        source_file: './data/example_dots.json',
-        target_file: './data/example_bars.json'
-    },
-    {
-        source_file: './data/example_bars.json',
-        target_file: './data/example_dots.json'
-    },
-    {
-        source_file: './data/leondutoit_dots.json',
-        target_file: './data/example_dots.json'
-    },
-    {
-        source_file: './data/leondutoit_dots.json',
-        target_file: './data/example_bars.json'
-    },
-    {
-        source_file: './data/dowson_bars.json',
-        target_file: './data/leondutoit_dots.json'
-    },
-    {
-        source_type: 'json_data',
-        source_file: './data/iris.data.json',
-        target_file: './data/leondutoit_dots.json'
-    },
-    {
-        source_file: './data/my_line.json',
         target_file: './data/kuijjer_line.json'
     },
     {
-        source_file: './data/kuijjer_line.json',
+        source_file: './data/cars_rollup3.data.json',
+        source_type: 'json_data',
         target_file: './data/my_line.json'
     },
-    {
-        source_type: 'vegalite',
-        source_file: './data/linechart.vegalite.json',
-        target_file: './data/kuijjer_line.json'
-    }
+     //IRIS EXAMPLES
+    //{
+    //    source_file: './data/iris.data.json',
+    //    source_type: 'json_data',
+    //    target_file: './data/vallandingham_scatter.json'
+    //},
+    //{
+    //    source_file: './data/iris.data.json',
+    //    source_type: 'json_data',
+    //    target_file: './data/weiglemc_scatter_2.json'
+    //},
+    //{
+    //    source_file: './data/iris.data.json',
+    //    source_type: 'json_data',
+    //    target_file: './data/leondutoit_dots.json'
+    //},
+    //{
+    //    source_file: './data/barley.data.json',
+    //    source_type: 'json_data',
+    //    target_file: './data/leondutoit_dots.json'
+    //},
+    //{
+    //    source_file: './data/purple_scatter.json',
+    //    target_file: './data/vallandingham_scatter.json'
+    //},
+    //{
+    //    source_file: './data/weiglemc_scatter_2.json',
+    //    target_file: './data/vallandingham_scatter.json'
+    //},
+    //{
+    //    source_file: './data/vallandingham_scatter.json',
+    //    target_file: './data/weiglemc_scatter_2.json'
+    //},
+    //{
+    //    source_file: './data/mbostock_bars.json',
+    //    target_file: './data/vallandingham_scatter.json'
+    //},
+    //{
+    //    source_file: './data/dowson_bars.json',
+    //    target_file: './data/mbostock_bars.json'
+    //},
+    //{
+    //    source_type: 'json_data',
+    //    source_file: './data/iris.data.json',
+    //    target_file: './data/mbostock_bars.json'
+    //},
+    //{
+    //    source_type: 'json_data',
+    //    source_file: './data/iris.data.json',
+    //    target_file: './data/dowson_bars.json'
+    //},
+    //{
+    //    source_file: './data/mbostock_bars.json',
+    //    target_file: './data/dowson_bars.json'
+    //},
+    //{
+    //    source_file: './data/weiglemc_scatter.json',
+    //    target_file: './data/dowson_bars.json'
+    //},
+    //{
+    //    source_file: './data/dowson_bars.json',
+    //    target_file: './data/weiglemc_scatter.json'
+    //},
+    //{
+    //    source_file: './data/mbostock_bars.json',
+    //    target_file: './data/excel_bars.json'
+    //},
+    //{
+    //    source_file: './data/excel_bars.json',
+    //    target_file: './data/mbostock_bars.json'
+    //},
+    //{
+    //    source_file: './data/mbostock_bars.json',
+    //    target_file: './data/leondutoit_dots.json'
+    //},
+    //{
+    //    source_file: './data/example_dots.json',
+    //    target_file: './data/example_bars.json'
+    //},
+    //{
+    //    source_file: './data/example_bars.json',
+    //    target_file: './data/example_dots.json'
+    //},
+    //{
+    //    source_file: './data/leondutoit_dots.json',
+    //    target_file: './data/example_dots.json'
+    //},
+    //{
+    //    source_file: './data/leondutoit_dots.json',
+    //    target_file: './data/example_bars.json'
+    //},
+    //{
+    //    source_file: './data/dowson_bars.json',
+    //    target_file: './data/leondutoit_dots.json'
+    //},
+    //{
+    //    source_type: 'json_data',
+    //    source_file: './data/iris.data.json',
+    //    target_file: './data/leondutoit_dots.json'
+    //},
+    //{
+    //    source_file: './data/my_line.json',
+    //    target_file: './data/kuijjer_line.json'
+    //},
+    //{
+    //    source_file: './data/kuijjer_line.json',
+    //    target_file: './data/my_line.json'
+    //},
+    //{
+    //    source_type: 'vegalite',
+    //    source_file: './data/linechart.vegalite.json',
+    //    target_file: './data/kuijjer_line.json'
+    //}
 ];
 
 module.exports = tests;
